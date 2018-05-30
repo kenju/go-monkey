c53fd1ed2fa4cd4d570a244e77c736fa0f63795a
Kenju Wagatsuma <ken901waga@gmail.com>
2018-05-31 08:50:14 +0900

    add keywords for == and !=

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -24,13 +24,27 @@ func (l *Lexer) NextToken() token.Token {
 
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.EQ, Literal: literal};
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
 	case '+':
 		tok = newToken(token.PLUS, l.ch)
 	case '-':
 		tok = newToken(token.MINUS, l.ch)
 	case '!':
-		tok = newToken(token.BANG, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.NOT_EQ, Literal: literal};
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
 	case '/':
 		tok = newToken(token.SLASH, l.ch)
 	case '*':
@@ -94,6 +108,27 @@ func (l *Lexer) skipWhitespace() {
 	}
 }
 
+// readChar gives the next character
+// and advance the position in the input string
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0 // no more chars to read
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition // set position
+	l.readPosition += 1 // advance to the next char
+}
+
+// readChar without moving pointers. just peek the char.
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0 // no more chars to peek
+	} else {
+		return l.input[l.readPosition]
+	}
+}
+
 func isLetter(ch byte) bool {
 	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
@@ -109,14 +144,3 @@ func newToken(tokenType token.TokenType, ch byte) token.Token {
 	}
 }
 
-// readChar gives the next character
-// and advance the position in the input string
-func (l *Lexer) readChar() {
-	if l.readPosition >= len(l.input) {
-		l.ch = 0 // no more chars to read
-	} else {
-		l.ch = l.input[l.readPosition]
-	}
-	l.position = l.readPosition // set position
-	l.readPosition += 1 // advance to the next char
-}
--- token/token.go
+++ token/token.go
@@ -19,6 +19,9 @@ const (
 	LT = "<"
 	GT = ">"
 
+	EQ = "=="
+	NOT_EQ = "!="
+
 	COMMA = ","
 	SEMICOLON = ";"
 
