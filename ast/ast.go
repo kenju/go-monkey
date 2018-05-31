467297592890cf026972525ffe070f8de9012d4d
Kenju Wagatsuma <ken901waga@gmail.com>
2018-05-31 21:48:02 +0900

    add Program, LetStatement and Identifier for ast package

--- ast/ast.go
+++ ast/ast.go
@@ -1,6 +1,9 @@
 package ast
 
+import "github.com/kenju/go-monkey/token"
+
 type Node interface {
+	// used only for debugging and testing
 	TokenLiteral() string
 }
 
@@ -12,4 +15,42 @@ type Statement interface {
 type Expression interface {
 	Node
 	expressionNode()
+}
+
+// Program node is the root node of every AST parser produces.
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name *Identifier // Name to hold the identifier of the biding
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {
+}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {
+}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
 }
