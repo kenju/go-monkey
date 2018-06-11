bdbc41a5488476e50725a2455212466b6b613124
Kenju Wagatsuma <ken901waga@gmail.com>
2018-06-11 22:29:45 +0900

    fix ast_test

--- ast/ast_test.go
+++ ast/ast_test.go
@@ -10,21 +10,14 @@ func TestString(t *testing.T) {
 	program := &Program{
 		Statements: []Statement{
 			&LetStatement{
-				Token: token.Token{
-					Type:    token.LET,
-					Literal: "let",
-				},
+				Token: token.Token{Type: token.LET, Literal: "let"},
 				Name: &Identifier{
-					Token: token.Token{
-						Type:    token.IDENT,
-						Literal: "myVar",
-					},
+					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+					Value: "myVar",
 				},
 				Value: &Identifier{
-					Token: token.Token{
-						Type:    token.IDENT,
-						Literal: "anotherVar",
-					},
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+					Value: "anotherVar",
 				},
 			},
 		},
