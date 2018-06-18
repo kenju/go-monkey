43cc3a423eeeefcc8b1ccd20cf99abc92cb1e0e9
Kenju Wagatsuma <ken901waga@gmail.com>
2018-06-18 22:46:59 +0900

    add HashKey and HashPair object

--- object/object.go
+++ object/object.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/kenju/go-monkey/ast"
+	"hash/fnv"
 )
 
 func NewEnclosedEnvironment(outer *Environment) *Environment {
@@ -49,6 +50,7 @@ const (
 	STRING_OBJ       = "STRING"
 	BUILTIN_OBJ      = "BUILTIN"
 	ARRAY_OBJ        = "ARRAY"
+	HASH_OBJ = "HASH"
 )
 
 type Object interface {
@@ -154,6 +156,62 @@ func (ao *Array) Inspect() string {
 	out.WriteString("[")
 	out.WriteString(strings.Join(elements, ", "))
 	out.WriteString("]")
+
+	return out.String()
+}
+
+type HashKey struct {
+	Type ObjectType
+	Value uint64
+}
+
+func (b *Boolean) HashKeys() HashKey {
+	var value uint64
+
+	if b.Value {
+		value = 1
+	} else {
+		value = 0
+	}
+
+	return HashKey{ Type: b.Type(), Value: value }
+}
+
+func (i *Integer) HashKey() HashKey {
+	return HashKey{
+		Type: i.Type(),
+		Value: uint64(i.Value),
+	}
+}
+
+func (s *String) HashKey() HashKey {
+	h := fnv.New64a()
+	h.Write([]byte(s.Value))
+
+	return HashKey{ Type: s.Type(), Value: h.Sum64() }
+}
+
+type HashPair struct {
+	Key Object
+	Value Object
+}
+
+type Hash struct {
+	Pairs map[HashKey]HashPair
+}
+
+func (h *Hash) Type() ObjectType { return HASH_OBJ }
+func (h *Hash) Inspect() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+	for _, pair := range h.Pairs {
+		pairs = append(pairs, fmt.Sprintf("%s: %s", pair.Key.Inspect(), pair.Value.Inspect()))
+	}
+
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
 
 	return out.String()
 }
