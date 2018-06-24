53e61baa2d61c66df1f3f1d89f7e07df6c462134
Kenju Wagatsuma <ken901waga@gmail.com>
2018-06-25 06:05:35 +0900

    Modify() suppor If, Block and Return statements

--- ast/modify.go
+++ ast/modify.go
@@ -22,6 +22,21 @@ func Modify(node Node, modifier ModifierFunc) Node {
 	case *IndexExpression:
 		node.Left, _ = Modify(node.Left, modifier).(Expression)
 		node.Index, _ = Modify(node.Index, modifier).(Expression)
+
+	case *IfExpression:
+		node.Condition, _ = Modify(node.Condition, modifier).(Expression)
+		node.Consequence, _ = Modify(node.Consequence, modifier).(*BlockStatement)
+		if node.Alternative != nil {
+			node.Alternative, _ = Modify(node.Alternative, modifier).(*BlockStatement)
+		}
+
+	case *BlockStatement:
+		for i, _ := range node.Statements {
+			node.Statements[i], _ = Modify(node.Statements[i], modifier).(Statement)
+		}
+
+	case *ReturnStatement:
+		node.ReturnValue, _ = Modify(node.ReturnValue, modifier).(Expression)
 	}
 
 	// calls the modifier with the given Node and **returns** the result
