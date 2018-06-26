c6e9413cead010211cafc00b7ea66f69ecc24a5e
Kenju Wagatsuma <ken901waga@gmail.com>
2018-06-27 06:18:57 +0900

    add MACRO keyword

--- token/token.go
+++ token/token.go
@@ -45,6 +45,7 @@ const (
 	IF       = "IF"
 	ELSE     = "ELSE"
 	RETURN   = "RETURN"
+	MACRO    = "MACRO"
 )
 
 type TokenType string
@@ -62,6 +63,7 @@ var keywords = map[string]TokenType{
 	"if":     IF,
 	"else":   ELSE,
 	"return": RETURN,
+	"macro":  MACRO,
 }
 
 func LookupIdent(ident string) TokenType {
