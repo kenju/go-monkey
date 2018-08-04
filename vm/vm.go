4aca62cd7b8b0e28d3d53cd48d468b6ad5173334
Kenju Wagatsuma <ken901waga@gmail.com>
2018-08-04 21:08:27 +0900

    add OpPop

--- vm/vm.go
+++ vm/vm.go
@@ -52,6 +52,9 @@ func (vm *VM) Run() error {
 
 			result := leftValue + rightValue
 			vm.push(&object.Integer{Value: result})
+
+		case code.OpPop:
+			vm.pop()
 		}
 	}
 	return nil
@@ -79,4 +82,9 @@ func (vm *VM) pop() object.Object {
 	o := vm.stack[vm.sp-1]
 	vm.sp--
 	return o
+}
+
+// LastPoppedStackElem is a test-only method
+func (vm *VM) LastPoppedStackElem() object.Object {
+	return vm.stack[vm.sp]
 }
