f43a83ce87a9e0229fde111c700c9e282b47453a
Kenju Wagatsuma <ken901waga@gmail.com>
2018-08-04 13:07:28 +0900

    add compiler/compiler.go

---
+++ compiler/compiler.go
@@ -0,0 +1,35 @@
+package compiler
+
+import (
+	"github.com/kenju/go-monkey/code"
+	"github.com/kenju/go-monkey/object"
+	"github.com/kenju/go-monkey/ast"
+)
+
+type Compiler struct {
+	instructions code.Instructions
+	constants []object.Object
+}
+
+func New() *Compiler {
+	return &Compiler{
+		instructions: code.Instructions{},
+		constants: []object.Object{},
+	}
+}
+
+func (c *Compiler) Compile(node ast.Node) error {
+	return nil
+}
+
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.instructions,
+		Constants: c.constants,
+	}
+}
+
+type Bytecode struct {
+	Instructions code.Instructions
+	constants []object.Object
+}
