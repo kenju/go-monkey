04b702a342f5c0b494a7507e3d00caa07b0381a6
Kenju Wagatsuma <ken901waga@gmail.com>
2018-08-04 20:17:41 +0900

    implement VM.Run

--- vm/vm.go
+++ vm/vm.go
@@ -4,6 +4,7 @@ import (
 	"github.com/kenju/go-monkey/compiler"
 	"github.com/kenju/go-monkey/object"
 	"github.com/kenju/go-monkey/code"
+	"fmt"
 )
 
 const StackSize = 2048
@@ -26,9 +27,40 @@ func New(bytecode *compiler.Bytecode) *VM {
 	}
 }
 
-func (v *VM) Run() error {
+func (vm *VM) Run() error {
+	// iterate through the whole instructions
+	for ip := 0; ip < len(vm.instructions); ip++ {
+		// convert one of instruction to an Opcode
+		op := code.Opcode(vm.instructions[ip])
+
+		switch op {
+		case code.OpConstant:
+			constIndex := code.ReadUint16(vm.instructions[ip+1:])
+			ip += 2
+
+			err := vm.push(vm.constants[constIndex])
+			if err != nil {
+				return err
+			}
+		}
+	}
 	return nil
 }
 
-func (v *VM) StackTop() {
+func (vm *VM) StackTop() object.Object {
+	if vm.sp == 0 {
+		return nil
+	}
+	return vm.stack[vm.sp-1] // -1 because sp(stack pointer) always points to the next value
+}
+
+func (vm *VM) push(o object.Object) error {
+	if vm.sp >= StackSize {
+		return fmt.Errorf("stack overflow")
+	}
+
+	vm.stack[vm.sp] = o
+	vm.sp ++
+
+	return nil
 }
